fc710098132f4eea80d44a12cebe1bc3c8d469da
cybertekschool <github@cybertekschool.com>
2020-02-13 12:59:30 -0500

    first class

--- src/test/java/com/automation/tests/Day1.java
+++ src/test/java/com/automation/tests/Day1.java
@@ -1,4 +1,25 @@
 package com.automation.tests;
 
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 public class Day1 {
+
+    @Test
+    public void test1(){
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        //since we use android, put android. could be IOS
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        //version of your device
+        desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "7.0");
+        //name of the device
+        //if it's a real device, you need to use udid parameter
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
+        //either you can specify app --> //path/to/the/appp.apk
+        //or, if app is already installed, you need to specify appActivity and AppPackage
+        //this info you can find in the internet, at work - from developers
+        //otherwise, you can use apk info app to find this information
+    }
 }
