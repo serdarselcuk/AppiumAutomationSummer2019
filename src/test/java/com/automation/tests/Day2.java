10b00c9b3362d5c3bf3a6c9b870ec1882a1054ac
cybertekschool <github@cybertekschool.com>
2020-02-14 16:36:56 -0500

    first class

--- src/test/java/com/automation/tests/Day2.java
+++ src/test/java/com/automation/tests/Day2.java
@@ -1,12 +1,16 @@
 package com.automation.tests;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -15,11 +19,11 @@ public class Day2 {
     private AndroidDriver<MobileElement> driver;
 
     @Before
-    public void setup(){
+    public void setup() {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
         desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "7.0");
-        desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/api_demos.apk");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "/api_demos.apk");
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
         desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         try {
@@ -30,13 +34,33 @@ public class Day2 {
     }
 
     @After
-    public void teardown(){
+    public void teardown() {
         driver.closeApp();
     }
 
 
+    @Test
+    public void test() throws Exception {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Views")));
 
-    public void test(){
+        driver.findElement(MobileBy.AccessibilityId("Views")).click();
+
+        MobileElement webview = driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable("
+                + "new UiSelector().scrollable(true)).scrollIntoView("
+                + "new UiSelector().textContains(\"WebView\"));"));
+
+        //textContains or text or id, className
+        webview.click();
+
+        //go back
+        driver.navigate().back();
+
+        MobileElement imageSwitcher = driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable("
+                + "new UiSelector().scrollable(true)).scrollIntoView("
+                + "new UiSelector().textContains(\"ImageSwitcher\"));"));
+
+        imageSwitcher.click();
 
     }
 }
