b98efc60b5a2f2945148c4866de1f14befc4c5ad
serdarselcuk <serdarselcukus@outlook.com>
2020-04-28 00:06:24 -0500

    last

---
+++ src/test/java/com/automation/tests/utils/serdar.java
@@ -0,0 +1,28 @@
+package com.automation.tests.utils;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class serdar {
+
+    @Test(dataProvider = "provide")
+    public void test(Object[][] a){
+
+        System.out.println("newsa");
+
+    }
+
+
+    @DataProvider(name = "provide")
+    public Object[][] provide(){
+
+        Object[][] kazim = {{12},{12}};
+
+    	 return kazim  
+     
+       }
+}
